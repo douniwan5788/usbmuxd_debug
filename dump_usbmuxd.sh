f48100083da68c824899009f4abde806cce2e03b
douniwan5788 <douniwan5788@gmail.com>
2022-04-06 15:38:04 +0800

    🐞fix: cleanup on error exit

--- dump_usbmuxd.sh
+++ dump_usbmuxd.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-
 export PATH="$PATH:/Applications/Wireshark.app/Contents/MacOS/tshark"
+
 # Parameters
 socket="/var/run/usbmuxd"
 dump=$1
@@ -11,12 +11,25 @@ port=9876
 source_socket="$(dirname "${socket}")/$(basename "${socket}").orig"
 
 # Move socket files
-mv "${socket}" "${source_socket}"
-trap "{ rm '${socket}'; mv '${source_socket}' '${socket}'; }" EXIT
+sudo mv "${socket}" "${source_socket}"
+
+function recover() {
+    echo "Recover"
+    # kill socat
+    jobs -p
+    sudo kill $(jobs -p)
+
+    sudo mv ${source_socket} ${socket}
+}
+
+trap recover EXIT
 
 # Setup pipe over TCP that we can tap into
 socat -t100 "TCP-LISTEN:${port},reuseaddr,fork" "UNIX-CONNECT:${source_socket}" &
-socat -t100 "UNIX-LISTEN:${socket},mode=777,reuseaddr,fork" "TCP:localhost:${port}" &
+sudo socat -t100 "UNIX-LISTEN:${socket},mode=777,reuseaddr,fork" "TCP:localhost:${port}" &
 
-# Record traffic
+echo "Started"
+# # Record traffic
 tshark -i lo0 -w "${dump}" -F pcapng "dst port ${port} or src port ${port}"
+# or wait forever and use wireshark
+# while :; do sleep 2073600; done
